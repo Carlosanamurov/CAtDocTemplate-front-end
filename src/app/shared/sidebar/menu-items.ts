4bc1224ded31e017dac4de7e0664ebb1221c7116
Carlosanamurov <carlosanamuroventura@upeu.edu.pe>
2021-06-25 12:33:14 -0500

    template 1

---
+++ src/app/shared/sidebar/menu-items.ts
@@ -0,0 +1,85 @@
+import { RouteInfo } from './sidebar.metadata';
+
+export const ROUTES: RouteInfo[] = [
+  {
+    path: '',
+    title: 'Personal',
+    icon: 'mdi mdi-dots-horizontal',
+    class: 'nav-small-cap',
+    extralink: true,
+    submenu: []
+  },
+  {
+    path: '/dashboard',
+    title: 'Dashboard',
+    icon: 'mdi mdi-gauge',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '',
+    title: 'UI Components',
+    icon: 'mdi mdi-dots-horizontal',
+    class: 'nav-small-cap',
+    extralink: true,
+    submenu: []
+  },
+  {
+    path: '/component/card',
+    title: 'Card',
+    icon: 'mdi mdi-blur-radial',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/accordion',
+    title: 'Accordion',
+    icon: 'mdi mdi-equal',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/alert',
+    title: 'Alert',
+    icon: 'mdi mdi-message-bulleted',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/carousel',
+    title: 'Carousel',
+    icon: 'mdi mdi-view-carousel',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/dropdown',
+    title: 'Dropdown',
+    icon: 'mdi mdi-arrange-bring-to-front',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/modal',
+    title: 'Modal',
+    icon: 'mdi mdi-tablet',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/pagination',
+    title: 'Pagination',
+    icon: 'mdi mdi-backburger',
+    class: '',
+    extralink: false,
+    submenu: []
+  }
+  
+];
